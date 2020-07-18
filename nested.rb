bff94f367269276613778d0aefbf2acf75397c20
Sarah Rogers <s.rogers0715@yahoo.com>
2020-07-18 03:39:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -36,9 +36,6 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-  alans_new_info = "GUI"
-programmer_hash[:alan_kay][:known_for] = alans_new_info
-programmer_hash
 end
 
 def dennis_ritchies_language
@@ -104,7 +101,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+  programmer_hash[:alan_kay][:known_for] = alans_new_info
+  programmer_hash
+    
 end
 
 def adding_to_dennis
